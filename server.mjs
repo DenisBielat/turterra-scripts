d8832a8550496045321a1e01119ab0048a79b124
DenisBielat <81764089+DenisBielat@users.noreply.github.com>
2024-10-06 17:21:57 -0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -14,13 +14,13 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_CLOUD_SECRET
 });
 
-// Middleware to add CORS headers
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// });
+Middleware to add CORS headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 // CORS configuration
 const corsOptions = {
