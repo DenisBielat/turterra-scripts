8237881ad23e12e609cd0c7b1b7f801527baf8e2
DenisBielat <81764089+DenisBielat@users.noreply.github.com>
2024-10-06 18:01:31 -0500

    Updated paths

--- script-loader.js
+++ script-loader.js
@@ -1,7 +1,7 @@
 (function() {
   const scripts = [
-    'https://turterra.vercel.app/physical-features-slider.js',
-    'https://turterra.vercel.app/distribution-map-script.js'
+    'https://turterra.vercel.app/public/physical-features-slider.js',
+    'https://turterra.vercel.app/public/distribution-map-script.js'
   ];
   
   scripts.forEach(src => {
