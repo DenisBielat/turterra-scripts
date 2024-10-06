eb2673c95ba98db42da5e72f64a0ed068266ff1a
DenisBielat <81764089+DenisBielat@users.noreply.github.com>
2024-10-06 17:23:27 -0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -14,14 +14,6 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_CLOUD_SECRET
 });
 
-Middleware to add CORS headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
 // CORS configuration
 const corsOptions = {
   origin: ['https://www.turterra.com', 'https://turterra.com'],
