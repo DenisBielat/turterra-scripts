7d0d48cc39baeec403a4049efed63262f3eb3245
DenisBielat <81764089+DenisBielat@users.noreply.github.com>
2024-10-09 15:28:48 -0500

    Add fetch turtle species script

--- public/script-loader.js
+++ public/script-loader.js
@@ -1,8 +1,9 @@
 (function() {
   const scripts = [
+    'https://turterra.vercel.app/fetch-turtle-species.js',
     'https://turterra.vercel.app/physical-features-slider.js',
     'https://turterra.vercel.app/distribution-map-script.js',
-    'https://turterra.vercel.app/turtle-profile-hero-slider.js',
+    'https://turterra.vercel.app/turtle-profile-hero-slider.js'
   ];
   
   scripts.forEach(src => {
